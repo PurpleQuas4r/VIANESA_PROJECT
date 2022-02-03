cb4cb8dc18335bd628a80b0fb4881fb47d42bde5
PurpleQuas4r <miguel.donoso.silvestre@gmail.com>
2022-02-03 13:55:48 -0300

    Beginning
    Aqui empieza esta mierda, la historia de la vianesa asesina.

---
+++ Assets/Scripts/player/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController characterController;
+    
+    public float speed = 47f;
+
+    public float gravity = -16.81f;
+
+//CAMBIAR VALORES EN UNITY, NO SE CAMBIARAN POR EL SCRIPT
+    public Transform groundCheck;
+    public float sphereRadius = 0.3f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+
+    Vector3 velocity;  
+
+    public float jumpHeight = 10;  
+
+    void Update()
+    {
+        //checkear si la vianesa esta en el suelo
+        isGrounded = Physics.CheckSphere(groundCheck.position,sphereRadius,groundMask);
+        
+        if (isGrounded && velocity.y <= -10 ) 
+        {
+            velocity.y = -10f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move= transform.right * x + transform.forward * z;
+
+
+        if (characterController.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        characterController.Move(move * speed *Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
+
+    }
+}
