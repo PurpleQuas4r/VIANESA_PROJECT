26989941c8a00b6c1007c49e76ba0158daf56036
kamus1 <benjamincamus9@gmail.com>
2022-02-06 04:19:49 -0300

    UI, granada, plataforma culia
    ui de la arma con el contador de balas
    ui de la vida pero no funciona (solo diseño)
    efecto de la granda , no tiene modelo
    plataforma (genera muchos bugs)

--- Assets/Scripts/World/GameManager.cs
+++ Assets/Scripts/World/GameManager.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Text ammoText;
+
+    public Text healthText;
+
+    public int health = 100;
+
     public static GameManager Instance { get; private set; }
 
-    public int gunAmmo = 10;
+    public int gunAmmo = 6;
 
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Update()
+    {
+        ammoText.text = gunAmmo.ToString();
+        healthText.text = health.ToString();
     }
 }
